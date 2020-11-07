34cd8ba42715446a0fca60aff5ecf6451e7b9437
Thk <th.perche@gmail.com>
2020-11-07 16:29:00 +0100

    Adding state & axios

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 import LoginForm from './Login/LoginForm';
 
 class App extends React.Component {
@@ -11,6 +12,21 @@ class App extends React.Component {
 
     //Way to make sure that we can use this on a class
     this.updateCurrentUser = this.updateCurrentUser.bind(this);
+  }
+
+  //When the component is mounted, we're gonna fetch the data from the API
+  componentDidMount(){
+    const url = 'http://localhost:5000/home'
+
+    axios.get(url)
+      .then((res) => {
+        this.setState({
+          //Do some
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 
   updateCurrentUser(item){
