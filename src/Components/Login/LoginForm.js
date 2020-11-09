d8c6134f33eb3f8cf22f7e0351047bb8f0301fc9
Thk <th.perche@gmail.com>
2020-11-09 23:14:14 +0100

    adding form

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import axios from 'axios';
 
 class LoginForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
+    submitForm = (event) => {
+        event.preventDefault();
+
+        axios.post('http://localhost:5000/api/justify',
+        {
+            headers: {
+                'Content-Type': "application/json",
+                "Authorization": "Basic " + Buffer.from("credentials.clientId" + ':' + "coucou").toString('base64')
+            },
+            data:{
+                text: this.textJustify.value
+            }
+        })
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
     }
     render() { 
-        return ( <div>
-            PlayerForm
-        </div> );
+        return ( 
+        <form className="col s12" onSubmit={this.submitForm.bind(this)}>
+              <div className="row">
+                <div className="input-field col s6">
+                  <input id="input_number" type="number" data-length="2" placeholder="80"/>
+                  <label htmlFor="input_number">Number of spacing : </label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s12">
+                  <textarea id="textJustify" name="textJustify" className="materialize-textarea" data-length="1000" placeholder="Paste your text here" ></textarea>
+                  <label htmlFor="textJustify" >Texte à justifier :</label>
+                </div>
+              </div>
+              <button className="btn waves-effect waves-light" type="submit"
+              name="action">Submit</button>
+        </form>
+        );
     }
 }
  
--- src/Components/User/UsersList.js
+++ src/Components/User/UsersList.js
@@ -6,7 +6,7 @@ const UsersList = (props) => {
         <ul className="collection">
             <li className="collection-header"><h4>Users fetched from API</h4></li>
             {props.users.map((user) => {
-                return <li className="collection-item" classNamekey={user._id}>
+                return <li className="collection-item" key={user._id}>
                 <span className="title" key={user._id}><strong>ID : </strong> {user._id}</span>
                 <br />
                 <p><strong>Email : </strong>{user.email}</p>
