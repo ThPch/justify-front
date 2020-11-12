8f50125975fc218c0c026d2dfbbb0d819e91882b
Thk <th.perche@gmail.com>
2020-11-12 21:02:14 +0100

    fixing form

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -11,29 +11,21 @@ class LoginForm extends React.Component {
         this.submitForm = this.submitForm.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
-    submitForm = (event) => {
+    submitForm = async (event) => {
         event.preventDefault();
         let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRpY3RhY0B0cmlwLmNvbSIsInBhc3N3b3JkIjoiJDJhJDA4JE1Nc2tTR2w1YUxFbVZzemNtc3d4d08xVjMueUVXMzkwbmliZFFjckFpLkZqcDA2ckxUTWRxIiwiaWF0IjoxNjA1MTc5NDc0fQ.zoXUdP4mfvVEwYm94xOb5D6-GWmQ06gd0tJvvhOPUOc"
         axios.defaults.headers.common['Authorization'] =`Bearer ${accessToken}`;
-        axios.post('http://localhost:5000/api/justify',
-        {
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type' : 'application/json',
-                'Accept' : 'application/json',
-
-            },
-            crossdomain: true,
-            body:{
-                data: this.state.textJustify
-            }
-        })
-        .then((response) => {
-            console.log(response);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+        
+        try{
+            const response = await axios.post('http://localhost:5000/api/justify',{data: this.state.textJustify})
+            const data = await response.data
+            console.log(data)
+        }
+        catch(err){
+            console.log(err)
+        }
+        
+        
     }
 
     handleChange(event) {
